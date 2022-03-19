10f1c20de3ed8bd0ccef3210b317031cfd0e95fe
kukina622 <wyes91622@gmail.com>
2022-03-20 01:28:39 +0800

    add Navigation

---
+++ src/components/layout/Navigation.tsx
@@ -0,0 +1,46 @@
+import {
+  Navbar,
+  Container,
+  Nav,
+  Image,
+  Form,
+  InputGroup,
+  Button,
+} from "react-bootstrap";
+import { BsSearch } from "react-icons/bs";
+import styled from "styled-components";
+
+const SearchButton = styled.button`
+  background-color: transparent;
+  border: none;
+`;
+
+function Navigation() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Nav className="me-auto my-1">
+          <Image
+            className="me-2"
+            src="https://avatars.githubusercontent.com/u/80891249?v=4"
+            roundedCircle
+            height={30}
+          />
+          <InputGroup>
+            <Form.Control
+              size="sm"
+              type="text"
+              placeholder="username"
+              className="bg-transparent text-white border-0 border-bottom"
+            />
+            <SearchButton type="submit">
+              <BsSearch color="white" size={18}/>
+            </SearchButton>
+          </InputGroup>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
